aa9fbdbc8975fc82b4f6a43d752be1e0c7fe34e3
acc <acc2gml@gmail.com>
2020-09-24 21:44:39 +0800

    Func Gcd Lcm

---
+++ ConsoleApp/ConsoleApp/Func.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp
+{
+	public static class Func
+	{
+        public static int Gcd(int a, int b)
+        {
+            return b == 0 ? a : Gcd(b, a % b);
+        }
+
+        public static int Gcds(params int[] nums)
+        {
+            return nums.Aggregate(Gcd);
+        }
+
+        public static int Lcm(int a, int b)
+        {
+            return a * b / Gcd(a, b);
+        }
+
+        public static int Lcms(params int[] nums)
+        {
+            return nums.Aggregate(Lcm);
+        }
+
+    }
+}
