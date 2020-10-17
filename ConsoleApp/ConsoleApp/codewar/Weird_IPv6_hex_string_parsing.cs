3fc05363bf0e9251386ccf566bee0074f6b25ff2
acc <acc2gml@gmail.com>
2020-10-17 15:38:16 +0800

    add new adj folder

---
+++ ConsoleApp/ConsoleApp/codewar/Weird_IPv6_hex_string_parsing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp
+{
+	public class Weird_IPv6_hex_string_parsing
+	{
+		public static string ParseIPv6(string iPv6)
+			=> string.Concat(Regex.Split(iPv6, @"[^0-9A-F]").Select(x => ConvertBlock(x)));
+
+		static int ConvertBlock(string s) => s.Select(c => Convert.ToInt32(c.ToString(), 16)).Sum();
+	}
+}
