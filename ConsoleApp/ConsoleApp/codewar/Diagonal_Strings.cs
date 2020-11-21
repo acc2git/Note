8a905143dd0670b55591862e352009a35907ef3b
acc <acc2gml@gmail.com>
2020-11-21 15:43:46 +0800

    Diagonal_Strings

---
+++ ConsoleApp/ConsoleApp/codewar/Diagonal_Strings.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp.codewar
+{
+	public class Diagonal_Strings
+	{
+		public static string[] DiagonalsOfSquare(string[] array)
+		{
+			if (array.Length == 0 || array.Any(x => x.Length != array.Length))
+				return null;
+
+			var sortedArray = array.OrderBy(x => x).ToArray();
+
+			var idxMapping = sortedArray
+				.Select((x, i) => new { str = x, idx = i })
+				.GroupBy(x => x.str, g => g.idx)
+				.ToDictionary(g => g.Key, g => new Queue<int>(g.ToArray()));
+
+			return array.Select(str => GetDiagonal(sortedArray, idxMapping[str].Dequeue(), 0)).ToArray();
+		}
+
+		static string GetDiagonal(string[] square, int i, int j)
+		{
+			string result = "";
+			int len = square.Length, cnt = 0;
+			while (cnt < len)
+			{
+				result += square[(i + cnt) % len][(j + cnt) % len];
+				cnt++;
+			}
+			return result;
+		}
+	}
+}
---
+++ ConsoleApp/NUnitTestProject1/Diagonal_Strings_Test.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleApp.codewar;
+using NUnit.Framework;
+
+namespace Test
+{
+	[TestFixture]
+	public class Diagonal_Strings_Test
+	{
+        [Test]
+        public void Test()
+        {
+            string[] test1 = new string[] { "abcd", "kata", "1234", "qwer" };
+            string[] expected1 = new string[] { "aae4", "kw3d", "1btr", "q2ca" };
+            Assert.AreEqual(expected1, Diagonal_Strings.DiagonalsOfSquare(test1));
+
+            string[] test2 = new string[] { "1a8er", "B36jh", "AiYe3", "B1t0a", "g47uj" };
+            string[] expected2 = new string[] { "1itjj", "B48ea", "A16ur", "B37e3", "gaY0h" };
+            Assert.AreEqual(expected2, Diagonal_Strings.DiagonalsOfSquare(test2));
+
+            string[] test3 = new string[] { "ab", "12" };
+            string[] expected3 = new string[] { "a2", "1b" };
+            Assert.AreEqual(expected3, Diagonal_Strings.DiagonalsOfSquare(test3));
+
+            string[] test4 = new string[] { "xyz", "xyz", "xyz" };
+            string[] expected4 = new string[] { "xyz", "xyz", "xyz" };
+            Assert.AreEqual(expected4, Diagonal_Strings.DiagonalsOfSquare(test4));
+
+            string[] test5 = new string[] { "abcd", "kata", "abcd", "qwer" };
+            string[] expected5 = new string[] { "abtr", "kwcd", "aaed", "qbca" };
+            Assert.AreEqual(expected5, Diagonal_Strings.DiagonalsOfSquare(test5));
+        }
+    }
+}
