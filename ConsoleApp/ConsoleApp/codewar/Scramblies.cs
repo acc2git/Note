964f6b433eb1be46160bd6a5461a30246eeb3ef9
acc <acc2gml@gmail.com>
2021-11-18 23:19:51 +0800

    s

---
+++ ConsoleApp/ConsoleApp/codewar/Scramblies.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp.codewar
+{
+	public class Scramblies
+	{
+		public static bool Scramble(string str1, string str2)
+		{
+			// your code
+			return true;
+		}
+	}
+}
