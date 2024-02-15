88d8ad28a812d245f3274ceff21a3fbe564c8625
acc <acc2gml@gmail.com>
2024-02-15 23:35:05 +0800

    date_range_prefix

---
+++ ConsoleApp/ConsoleApp/practice/date_range_prefix.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp.practice
+{
+	public class DateRangePrefixSolution
+	{
+		public List<string> date_range_prefix(DateTime start, DateTime end)
+		{
+			// across multiple years -> start year, years between start and end, end year
+			if (start.Year != end.Year)
+				return date_range_prefix(start, new DateTime(start.Year, 12, 31))
+					.Concat(RangeBetween(start.Year + 1, end.Year - 1).Select(year => RtnResult(year)))
+					.Concat(date_range_prefix(new DateTime(end.Year, 1, 1), end))
+					.ToList();
+
+			// same year and full year
+			if (start.Month == 1 && start.Day == 1 && end.Month == 12 && end.Day == 31)
+				return new List<string> { RtnResult(start.Year) };
+
+			// same year and across multiple months -> start month, months between start and end, end month
+			var lastDayForStartMonth = EndOfMonth(start);
+			if (start.Month != end.Month)
+				return date_range_prefix(start, lastDayForStartMonth)
+					.Concat(RangeBetween(start.Month + 1, end.Month - 1).Select(month => RtnResult(start.Year, month)))
+					.Concat(date_range_prefix(new DateTime(end.Year, end.Month, 1), end))
+					.ToList();
+
+			// same year and same month and full month
+			if (start.Day == 1 && end.Day == EndOfMonth(end).Day)
+				return new List<string> { RtnResult(start.Year, start.Month) };
+
+			// same year and same month -> list days
+			return RangeBetween(start.Day, end.Day).Select(day => RtnResult(start.Year, start.Month, day)).ToList();
+		}
+
+		private IEnumerable<int> RangeBetween(int s, int e)
+		{
+			for (int i = s; i <= e; i++)
+				yield return i;
+		}
+
+		private string RtnResult(int y, int? m = null, int? d = null)
+		{
+			if (m == null && d == null)
+				return $"{y:0000}";
+			if (d == null)
+				return $"{y:0000}/{m:00}";
+			return $"{y:0000}/{m:00}/{d:00}";
+		}
+
+		private DateTime EndOfMonth(DateTime date)
+		{
+			return new DateTime(date.Year, date.Month, 1).AddMonths(1).AddDays(-1);
+		}
+	}
+}
---
+++ ConsoleApp/NUnitTestProject1/DateRangePrefixSolution_Test.cs
@@ -0,0 +1,42 @@
+﻿using ConsoleApp.practice;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Test
+{
+	[TestFixture]
+    public class DateRangePrefixSolution_Test
+	{
+        private static DateRangePrefixSolution sol = new DateRangePrefixSolution();
+
+        private static void testing(DateTime start, DateTime end, List<string> ans)
+        {
+            Assert.AreEqual(sol.date_range_prefix(start, end), ans);
+        }
+
+        [Test]
+        public static void test1()
+        {
+            testing(new DateTime(2021, 1, 1), new DateTime(2021, 12, 31), new List<string> { "2021" });
+            testing(new DateTime(2021, 1, 1), new DateTime(2022, 1, 1), new List<string> { "2021", "2022/01/01" });
+            testing(new DateTime(2021, 1, 1), new DateTime(2021, 1, 31), new List<string> { "2021/01" });
+            testing(new DateTime(2021, 1, 1), new DateTime(2021, 3, 1), new List<string> { "2021/01", "2021/02", "2021/03/01" });
+            testing(new DateTime(2021, 2, 1), new DateTime(2021, 2, 28), new List<string> { "2021/02" });
+            testing(new DateTime(2020, 2, 1), new DateTime(2020, 2, 28), DaysBetween(new DateTime(2020, 2, 1), new DateTime(2020, 2, 28)).ToList());
+            testing(new DateTime(2021, 11, 30), new DateTime(2023, 1, 1), new List<string> { "2021/11/30", "2021/12", "2022", "2023/01/01" });
+            testing(new DateTime(2021, 1, 5), new DateTime(2021, 1, 7), new List<string> { "2021/01/05", "2021/01/06", "2021/01/07" });
+        }
+
+        private static IEnumerable<string> DaysBetween(DateTime s, DateTime e)
+        {
+            DateTime c = s;
+            while (c <= e)
+            {
+                yield return c.ToString("yyyy/MM/dd");
+                c = c.AddDays(1);
+            }
+        }
+    }
+}
